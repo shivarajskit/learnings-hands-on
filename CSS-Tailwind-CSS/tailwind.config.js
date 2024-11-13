7926fd97c6cfd8d20c3673eed16c1375d41ebde5
Shivaraj S <shivarajskit@gmail.com>
2024-11-13 14:43:48 +0530

    custom supports and custom classes

--- CSS-Tailwind-CSS/tailwind.config.js
+++ CSS-Tailwind-CSS/tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,vue}", "./index.html"], // Update with the path to your files
   theme: {
     extend: {},
+    supports: {
+      grid: "display: grid", //custom supports-*
+    },
   },
   plugins: [],
 };
