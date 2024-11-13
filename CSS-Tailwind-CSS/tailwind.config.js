3d7ad1ac3aee2298cd4deb933657bfe2adbd1b15
Shivaraj S <shivarajskit@gmail.com>
2024-11-13 14:48:36 +0530

    custom selector dark mode on

--- CSS-Tailwind-CSS/tailwind.config.js
+++ CSS-Tailwind-CSS/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,vue}", "./index.html"], // Update with the path to your files
+  darkMode: "selector",
   theme: {
     extend: {},
     supports: {
