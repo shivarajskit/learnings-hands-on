94f78277fbd1e0b2ca7ab6135ac3a029abcd5966
Shivaraj S <shivarajskit@gmail.com>
2024-11-13 15:14:37 +0530

    using arbitrary values

--- CSS-Tailwind-CSS/tailwind.config.js
+++ CSS-Tailwind-CSS/tailwind.config.js
@@ -2,9 +2,36 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,vue}", "./index.html"], // Update with the path to your files
   darkMode: "selector",
   theme: {
-    extend: {},
     supports: {
       grid: "display: grid", //custom supports-*
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      colors: {
+        blue: "#1fb6ff",
+        pink: "#ff49db",
+        orange: "#ff7849",
+        green: "#13ce66",
+        "gray-dark": "#273444",
+        gray: "#8492a6",
+        "gray-light": "#d3dce6",
+      },
     },
   },
   plugins: [],
