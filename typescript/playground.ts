71176d6558423b2421abe3da3cb3b561c3332fe8
Shivaraj S <shivarajskit@gmail.com>
2024-11-23 10:00:21 +0530

    Usage of Generic Type

--- typescript/playground.ts
+++ typescript/playground.ts
@@ -1,9 +1,29 @@
 //1. Array Data Type - get first elemrnt of an array
-type Input = (number | string)[];
+// type Input = (number | string)[];
 
-function getFirstInput(array: Input): (number | string) {
+// function getFirstInput(array: Input): (number | string) {
+//   return array[0];
+// }
+
+// let firstElem = getFirstInput([1, 2, 3]);
+// let firstElemStr = getFirstInput(['one', 'two', 'three']);
+
+// console.log(firstElem);
+// console.log(firstElemStr);
+
+// there is a problem if we do like below
+// firstElemStr.toLowerCase(); // Property 'toLowerCase' does not exist on type 'string | number'.
+
+// To fix the problem, we can use generic type like below
+
+function getFirstInput<T>(array: T[]): T {
   return array[0];
 }
 
-console.log(getFirstInput([1, 2, 3]));
-console.log(getFirstInput(['one', 'two', 'three']));
+let firstElem = getFirstInput([1, 2, 3]);
+let firstElemStr = getFirstInput(['ONE', 'TWO', 'THREE']);
+
+console.log(firstElem);
+console.log(firstElemStr);
+console.log(firstElemStr.toLowerCase());
+// OUTPUT: 1, ONE, one
